ff86c58439127ae17dc1ef863426cf8d965a593b
Jitesh Srivastav <90095692+decifur@users.noreply.github.com>
2023-10-09 20:13:46 +0530

    added Hero Component

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import './Hero.css'
+import Header from '../Header/Header'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+        <div className='left-h'>
+            <Header />
+
+            {/* The best Ad */}
+            <div className='the-best-ad'>
+                <div></div>
+                <span>the best fitness club in the town</span>
+            </div>
+
+            {/* Hero Heading */}
+            <div className='hero-text'>
+                <div>
+                    <span className='stroke-text'>Shape </span>
+                    <span>Your</span>
+                </div>
+                <div>
+                    <span>Ideal body</span>
+                </div>
+                <div>
+                    <span>
+                        In here we will help you to shape and build your ideal body and live up your life to fullest
+                    </span>
+                </div>
+            </div>
+
+            {/* figures */}
+            <div className='figures'>
+                <div>
+                    <span>+140</span>
+                    <span>expert coaches</span>
+                </div>
+                <div>
+                    <span>+978</span>
+                    <span>members joined</span>
+                </div>
+                <div>
+                    <span>+50</span>
+                    <span>fitness programs</span>
+                </div>
+            </div>
+
+            {/* hero buttons */}
+            <div className='hero-buttons'>
+                <buttons className="btn">Get Started</buttons>
+                <buttons className="btn">Learn More</buttons>
+            </div>
+        </div>
+        <div className='right-h'>right</div>
+    </div>
+  )
+}
+
+export default Hero

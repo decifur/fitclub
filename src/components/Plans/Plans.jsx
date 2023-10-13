3536be8d6dde2d4eb8dc5255579f5e9aec4d501d
Jitesh Srivastav <90095692+decifur@users.noreply.github.com>
2023-10-13 23:16:20 +0530

    completed Plans.jsx

--- src/components/Plans/Plans.jsx
+++ src/components/Plans/Plans.jsx
@@ -1,30 +1,43 @@
-import React from 'react'
-import "./Plans.css"
-import {plansData} from "../../data/plansData"
+import React from "react";
+import "./Plans.css";
+import { plansData } from "../../data/plansData";
+import whiteTick from "../../assets/whiteTick.png";
 
 const Plans = () => {
   return (
-    <div className='plans-container'>
-        <div className='programs-header' style={{gap: "2rem"}}>
-            <span className='stroke-text'>READY TO START</span>
-            <span>YOUR JOURNEY
-</span>
-            <span className='stroke-text'>NOW WITHUS</span>
-        </div>
+    <div className="plans-container">
+      <div className="programs-header" style={{ gap: "2rem" }}>
+        <span className="stroke-text">READY TO START</span>
+        <span>YOUR JOURNEY</span>
+        <span className="stroke-text">NOW WITHUS</span>
+      </div>
 
-        {/* plans card */}
-        <div className='plan'>
-            {plansData.map((plan,i)=>{
-                <div className='plan'>
-                    {plan.icon}
-                    <span>{plan.name}</span>
-                    <span>{plan.price}</span>
+      {/* plans card */}
+      <div className="plans">
+        {plansData.map((plan, i) => (
+          <div className="plan" key={i}>
+            {plan.icon}
+            <span>{plan.name}</span>
+            <span>{plan.price}</span>
+            <div className="features">
+              {plan.features.map((feature, i) => (
+                <div className="feature">
+                  <img src={whiteTick} alt="" />
+                  <span key={i}>{feature}</span>
                 </div>
-            })}
-        </div>
-      
-    </div>
-  )
-}
+              ))}
+            </div>
 
-export default Plans
+            <div>
+                <span>See more benefits -></span>
+            </div>
+            <button className="btn">Join now</button>
+
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Plans;

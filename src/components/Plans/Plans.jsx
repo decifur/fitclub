f40652fff858b7c33a63e614428b223243bca34f
Jitesh Srivastav <90095692+decifur@users.noreply.github.com>
2023-10-14 19:19:32 +0530

    completed Plans.jsx

--- src/components/Plans/Plans.jsx
+++ src/components/Plans/Plans.jsx
@@ -29,7 +29,7 @@ const Plans = () => {
             </div>
 
             <div>
-                <span>See more benefits -></span>
+                <span>See more benefits →</span>
             </div>
             <button className="btn">Join now</button>
 

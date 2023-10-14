171fb696f4ebf61d60fc362ce2f47eb6981a012b
Jitesh Srivastav <90095692+decifur@users.noreply.github.com>
2023-10-14 21:45:28 +0530

    added new components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
+import Footer from './components/Footer/Footer';
 import Hero from './components/Hero/Hero';
+import Join from './components/Join/Join';
 import Plans from './components/Plans/Plans';
 import Programs from './components/Programs/Programs';
 import Reasons from './components/Reasons/Reasons';
@@ -12,6 +14,8 @@ function App() {
       <Reasons/>
       <Plans/>
       <Testimonials/>
+      <Join/>
+      <Footer/>
     </div>
   );
 }

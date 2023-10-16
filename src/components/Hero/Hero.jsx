c4608637a1d6d563c3f026a44ae3af29aadb5793
Jitesh Srivastav <90095692+decifur@users.noreply.github.com>
2023-10-16 14:53:20 +0530

    added blur in Hero component

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,89 +1,110 @@
-import React from 'react'
-import './Hero.css'
-import Header from '../Header/Header'
-import hero_image from "../../assets/hero_image.png"
-import hero_image_back from "../../assets/hero_image_back.png"
-import Heart from "../../assets/heart.png"
-import Calories from "../../assets/calories.png"
+import React from "react";
+import "./Hero.css";
+import Header from "../Header/Header";
+import hero_image from "../../assets/hero_image.png";
+import hero_image_back from "../../assets/hero_image_back.png";
+import Heart from "../../assets/heart.png";
+import Calories from "../../assets/calories.png";
+import { motion } from "framer-motion";
 
 const Hero = () => {
+  const transition = { type: "spring", duration: 3 };
+
   return (
-    <div className='hero'>
+    <div className="hero">
+      <div className="blur hero-blur"></div>
 
-        <div className='blur hero-blur'></div>
+      <div className="left-h">
+        <Header />
 
-        <div className='left-h'>
-            <Header />
-
-            {/* The best Ad */}
-            <div className='the-best-ad'>
-                <div></div>
-                <span>the best fitness club in the town</span>
-            </div>
-
-            {/* Hero Heading */}
-            <div className='hero-text'>
-                <div>
-                    <span className='stroke-text'>Shape </span>
-                    <span>Your</span>
-                </div>
-                <div>
-                    <span>Ideal body</span>
-                </div>
-                <div>
-                    <span>
-                        In here we will help you to shape and build your ideal body and live up your life to fullest
-                    </span>
-                </div>
-            </div>
-
-            {/* figures */}
-            <div className='figures'>
-                <div>
-                    <span>+ 140</span>
-                    <span>expert coaches</span>
-                </div>
-                <div>
-                    <span>+ 978</span>
-                    <span>members joined</span>
-                </div>
-                <div>
-                    <span>+ 50</span>
-                    <span>fitness programs</span>
-                </div>
-            </div>
-
-            {/* hero buttons */}
-            <div className='hero-buttons'>
-                <buttons className="btn">Get Started</buttons>
-                <buttons className="btn">Learn More</buttons>
-            </div>
+        {/* The best Ad */}
+        <div className="the-best-ad">
+          <motion.div
+            initial={{ left: "238px" }}
+            whileInView={{ left: "8px" }}
+            transition={{ ...transition, type: "tween" }}
+          ></motion.div>
+          <span>the best fitness club in the town</span>
         </div>
-        <div className='right-h'>
-            <button className='btn'>Join Now</button>
 
-            <div className='heart-rate'>
-                <img src={Heart} alt='heart-rate'/>
-                <span>Heart Rate</span>
-                <span>116 bpm</span>
-            </div>
-
-            {/* Hero Images */}
-            <img src={hero_image} className='hero-image' alt='hero'/>
-            <img src={hero_image_back} className='hero-image-back' alt='hero-back'/>
-
-            {/* Calories */}
-            <div className='calories'>
-                <img src={Calories} alt='calories' />
-                <div>
-                    <span>Calories Burned</span>
-                    <span>220 kcal</span>
-                </div>
-            </div>
-
+        {/* Hero Heading */}
+        <div className="hero-text">
+          <div>
+            <span className="stroke-text">Shape </span>
+            <span>Your</span>
+          </div>
+          <div>
+            <span>Ideal body</span>
+          </div>
+          <div>
+            <span>
+              In here we will help you to shape and build your ideal body and
+              live up your life to fullest
+            </span>
+          </div>
         </div>
+
+        {/* figures */}
+        <div className="figures">
+          <div>
+            <span>+ 140</span>
+            <span>expert coaches</span>
+          </div>
+          <div>
+            <span>+ 978</span>
+            <span>members joined</span>
+          </div>
+          <div>
+            <span>+ 50</span>
+            <span>fitness programs</span>
+          </div>
+        </div>
+
+        {/* hero buttons */}
+        <div className="hero-buttons">
+          <buttons className="btn">Get Started</buttons>
+          <buttons className="btn">Learn More</buttons>
+        </div>
+      </div>
+      <div className="right-h">
+        <button className="btn">Join Now</button>
+
+        <motion.div 
+        initial={{right: '-1rem'}}
+        whileInView={{right: '4rem'}}
+        transition={transition}
+        className="heart-rate">
+          <img src={Heart} alt="heart-rate" />
+          <span>Heart Rate</span>
+          <span>116 bpm</span>
+        </motion.div>
+
+        {/* Hero Images */}
+        <img src={hero_image} className="hero-image" alt="hero" />
+        <motion.img
+            initial={{right: '11rem'}}
+        whileInView={{right: '20rem'}}
+        transition={transition}
+          src={hero_image_back}
+          className="hero-image-back"
+          alt="hero-back"
+        />
+
+        {/* Calories */}
+        <motion.div 
+        initial={{right: '37rem'}}
+        whileInView={{right: '28rem'}}
+        transition={transition}
+        className="calories">
+          <img src={Calories} alt="calories" />
+          <div>
+            <span>Calories Burned</span>
+            <span>220 kcal</span>
+          </div>
+        </motion.div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;

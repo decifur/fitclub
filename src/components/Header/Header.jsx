40accb1233047e3c6f50af2175a3da8ffc9e6d94
Jitesh Srivastav <90095692+decifur@users.noreply.github.com>
2023-10-20 16:43:35 +0530

    upated header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,41 @@
-import './Header.css'
-import logo from "../../assets/logo.png"
+import "./Header.css";
+import logo from "../../assets/logo.png";
+import bars from "../../assets/bars.png";
+import { useState } from "react";
 
 const Header = () => {
-    return (
-        <div className='header'>
-            <img src={logo} alt='logo' className='logo'/>
+  const mobile = window.innerWidth <= 768 ? true : false;
+  const [menuOpened, setMenuOpened] = useState(false);
 
-            <ul className='header-menu'>
-                <li>Home</li>
-                <li>Programs</li>
-                <li>Why us</li>
-                <li>Plans</li>
-                <li>Testimonials</li>
-            </ul>
+  return (
+    <div className="header">
+      <img src={logo} alt="logo" className="logo" />
+      {menuOpened === false && mobile === true ? (
+        <div
+          style={{
+            backgroundColor: "var(--appColor)",
+            padding: "0.5rem",
+            borderRadius: "5px",
+          }}
+          onClick={() => setMenuOpened(true)}
+        >
+          <img
+            src={bars}
+            alt="menu-bar"
+            style={{ width: "1.5rem", height: "1.5rem" }}
+          />
         </div>
-    )
-}
+      ) : (
+        <ul className="header-menu">
+          <li onClick={() => setMenuOpened(false)}>Home</li>
+          <li onClick={() => setMenuOpened(false)}>Programs</li>
+          <li onClick={() => setMenuOpened(false)}>Why us</li>
+          <li onClick={() => setMenuOpened(false)}>Plans</li>
+          <li onClick={() => setMenuOpened(false)}>Testimonials</li>
+        </ul>
+      )}
+    </div>
+  );
+};
 
 export default Header;

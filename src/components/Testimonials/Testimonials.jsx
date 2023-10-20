8b2bb8bcd5badb487e1c62e89b0eb2547ea596f6
Jitesh Srivastav <90095692+decifur@users.noreply.github.com>
2023-10-20 16:45:49 +0530

    updated Testimonials

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -12,7 +12,7 @@ const Testimonials = () => {
   const transition = { type: "spring", duration: 3 };
 
   return (
-    <div className="Testimonials">
+    <div className="testimonials">
       <div className="left-t">
         <span>Testimonials</span>
         <span className="stroke-text">What they</span>
